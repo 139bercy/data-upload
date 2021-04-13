71e812da3b2fdf067ad88d28bb7131ddf87e0828
loldebyte <qdimarellis@laposte.net>
2021-04-13 15:34:28 +0200

    add onDrop functions

--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -2,12 +2,31 @@ import React, { Component } from "react";
 import axios from 'axios';
 
 import UserService from "../services/user.service";
-import Dropzone from "../Dropzone";
+import Dropzone from "react-dropzone";
 
-export default class Home extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
 
+    this.onDropRejected = (rejectedFiles) => {
+      rejectedFiles.forEach(file => {
+          console.log("Rejected ", file.name);
+      })
+    };
+  
+    this.onDropAccepted = (acceptedFiles) => {
+      acceptedFiles.forEach(file => {
+          console.log(file.name);
+          const formData = new FormData();
+          formData.append(file.name, file);
+          axios.post('http://localhost/api/upload', formData)
+          .then(res => {
+              console.log(res);
+          }
+          ).catch(err => console.log(err));
+      })
+    };
+    
     this.state = {
       content: ""
     };
@@ -31,27 +50,29 @@ export default class Home extends Component {
     );
   }
 
-  onDropAccepted = (acceptedFiles) => {
-    acceptedFiles.forEach(file => {
-      console.log(file.name);
-      const formData = new FormData();
-      formData.append(file);
-      axios.post('http://localhost/upload', formData)
-        .then(res => {
-          console.log(res);
-        }
-      ).catch(err => console.log(err))
-    })
-  }
-
   render() {
     return (
       <div className="container">
         <header className="jumbotron">
           <h3>Veuillez utiliser le cadre ci-dessous pour envoyer les fichiers de données</h3>
         </header>
-        <Dropzone onDropAccepted={this.onDropAccepted} accept={"text/csv"}/>
+        <Dropzone
+          onDropAccepted={this.onDropAccepted}
+          onDropRejected={this.onDropRejected}
+          accept={"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/csv"}
+          >
+            {({getRootProps, getInputProps}) => (
+              <section className="dropzone">
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
       </div>
     );
   }
 }
+
+export default Home;
