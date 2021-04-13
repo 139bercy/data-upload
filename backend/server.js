9872f41745ec5797334189a3b2fd7ae3cf436534
loldebyte <qdimarellis@laposte.net>
2021-04-13 09:29:11 +0200

    add post method

--- backend/server.js
+++ backend/server.js
@@ -69,3 +69,24 @@ async function initial() {
     console.log('Drop and Resync Db');
   }
 }
+
+app.post('/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = "~/uploads" + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
