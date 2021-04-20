3d3f3eca1cccc41f3b38fc4db905a9003a4de619
loldebyte <qdimarellis@laposte.net>
2021-04-20 10:50:30 +0200

    add interceptor logic to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import axios from 'axios';
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+const history = createBrowserHistory();
+axios.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response.status === 401 || error.response.status === 403) {
+      history.push('/login');
+  }
+  return Promise.reject(error);
+})
+
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={history}>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 
