2c1aad0125b5fdf961876b4dc600fc31df679ca6
loldebyte <qdimarellis@laposte.net>
2021-04-20 10:53:11 +0200

    use imported interceptor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,10 @@ import axios from 'axios';
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import httpInterceptor from './services/interceptor';
 
 const history = createBrowserHistory();
-axios.interceptors.response.use(response => {
-  return response;
-}, error => {
-  if (error.response.status === 401 || error.response.status === 403) {
-      history.push('/login');
-  }
-  return Promise.reject(error);
-})
-
+httpInterceptor(history);
 
 ReactDOM.render(
   <Router history={history}>
