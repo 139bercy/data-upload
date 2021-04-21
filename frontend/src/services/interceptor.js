6e12e251ed470073980811c579dbbb41cf4d724f
loldebyte <qdimarellis@laposte.net>
2021-04-21 09:24:54 +0200

    delete invalid authToken

--- frontend/src/services/interceptor.js
+++ frontend/src/services/interceptor.js
@@ -5,8 +5,14 @@ export default function setupInterceptors(history) {
     axios.interceptors.response.use(response => {
         return response;
     }, error => {
-        if (error.response.status === 401 || error.response.status === 403) {
+        if (error.response.status === 401) {
+            user = localStorage.getItem("user");
+            delete user.accessToken;
+            localStorage.setItem("user", user);
             history.push('/login');
+        }
+        if (error.response.status === 403) {
+            console.log("Unauthorized");
         }
         return Promise.reject(error);
     })
