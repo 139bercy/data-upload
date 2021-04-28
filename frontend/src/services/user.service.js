dcb24ec63be7367dc66b6d507e378bb61bbe7fac
loldebyte <qdimarellis@laposte.net>
2021-04-28 16:32:19 +0200

    add calls to user API

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -4,6 +4,9 @@ import authHeader from './auth-header';
 const API_URL = '/api/users';
 
 class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + '/all');
+  }
 
   getUserBoard() {
     return axios.get(API_URL + '/user', { headers: authHeader() });
