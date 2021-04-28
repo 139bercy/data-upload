38834a788adaa3d2d38735db265ce4750156dbe5
loldebyte <qdimarellis@laposte.net>
2021-04-28 16:32:51 +0200

    WIP use react-table to show user data

--- frontend/src/components/board-admin.component.js
+++ frontend/src/components/board-admin.component.js
@@ -3,41 +3,92 @@ import React, { Component } from "react";
 import UserService from "../services/user.service";
 import Table from "./table-component";
 
+function handleDeleteClick(user) {
+  console.log("Trying to delete :", user);
+  UserService.deleteUser(user);
+}
+
+function handleUpdateClick(user) {
+  console.log("Trying to update :", user);
+  UserService.updateUser(user);
+}
+
+function onCheckboxChange() {
+  console.log("NOT IMPLEMENTED");
+}
+
+const columns =
+  [
+    {
+      Header: "Nom d'utilisateur",
+      accessor: 'username',
+    },
+    {
+      Header: 'mail',
+      accessor: 'email',
+    },
+    {
+      Header: "Actif",
+      accessor: 'enable',
+      Cell: cell => (
+        <label>
+          <input
+            type="checkbox"
+            checked={cell.row.values.enable}
+            onChange={onCheckboxChange}
+          />
+        </label>
+      )
+    },
+    {
+      Header: "Supprimer",
+      Cell: cell => (
+        <button onClick={handleDeleteClick(cell.row.values.username)}>
+          Delete
+        </button>
+      )
+    },
+    {
+      Header: "Réinitialiser le mot de passe",
+      // button
+    }
+  ];
+
 export default class BoardAdmin extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      content: ""
+      users: []
     };
   }
 
   componentDidMount() {
-    UserService.getAdminBoard().then(
-      response => {
-        this.setState({
-          content: response.data
-          // TODO: SQL request through api around there, store it in state
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString()
-        });
-      }
-    );
+    if (this.state.users.length === 0) {
+      UserService.getUsers().then(
+        response => {
+          this.setState({
+            users: response.data
+          });
+        },
+        error => {
+          this.setState({
+            content:
+              (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+              error.message ||
+              error.toString()
+          });
+        }
+      );
+    }
   }
 
   render() {
     return (
-      // TODO: use the Table component with the state
       <div className="container">
-        <Table>???</Table>
+        <Table columns={columns} data={this.state.users} ></Table>
       </div>
     );
   }
