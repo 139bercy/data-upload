0e03039555cd78586271dbacb7b6f6adb231decb
loldebyte <qdimarellis@laposte.net>
2021-04-28 16:32:19 +0200

    add calls to user API

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,20 +1,32 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = '/api/test/';
+const API_URL = '/api/users';
 
 class UserService {
 
   getUserBoard() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
+    return axios.get(API_URL + '/user', { headers: authHeader() });
   }
 
   getModeratorBoard() {
-    return axios.get(API_URL + 'mod', { headers: authHeader() });
+    return axios.get(API_URL + '/mod', { headers: authHeader() });
   }
 
   getAdminBoard() {
-    return axios.get(API_URL + 'admin', { headers: authHeader() });
+    return axios.get(API_URL + '/admin', { headers: authHeader() });
+  }
+
+  getUsers() {
+    return axios.get(API_URL, { headers: authHeader() });
+  }
+
+  updateUser(user) {
+    return axios.put(API_URL + '/' + user.username, user, { headers: authHeader() });
+  }
+
+  deleteUser(user) {
+    return axios.delete(API_URL + '/' + user.username, { headers: authHeader() })
   }
 }
 
