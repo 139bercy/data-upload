aec1239b1f6deb10fa46c21a1d040f63719ea3a6
kaa-serpent <guillaumedecadoudal@ygmail.com>
2021-04-29 17:10:50 +0200

    verify if env exist

--- backend/app/controllers/upload.controller.js
+++ backend/app/controllers/upload.controller.js
@@ -1,8 +1,15 @@
 
+const db = require("../models");
+
+const Env = db.environnement;
+
 exports.upload = function(req, res) {
 
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
+    }   
+    if (Env.findOne({$where: {name: req.params.environnement}}) === null ) {
+        return res.status(400).send(req.params.environnement + ' is not a valid environnement');
     }
     let promises = Object.keys(req.files).map(filename => {
         console.log(filename);
