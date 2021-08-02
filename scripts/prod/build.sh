d74c6e96f35cadb24b4adf88a8ef297d934bfd08
Erwan Daubert <erwan.daubert@gmail.com>
2021-08-02 11:21:25 +0200

    Ajout d'une commande dans le build

--- scripts/prod/build.sh
+++ scripts/prod/build.sh
@@ -13,6 +13,7 @@ rm -rf deploy/data-upload-backend.tar
 rm -rf deploy/data-upload-nginx-frontend.tar
 
 # Save des images
+docker pull postgres:13-alpine
 docker save postgres:13-alpine -o deploy/postgres.tar
 docker save data-upload-backend:prod -o deploy/data-upload-backend.tar
 docker save data-upload-nginx-frontend:prod -o deploy/data-upload-nginx-frontend.tar
