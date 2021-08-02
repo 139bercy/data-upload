46dbc6ee2f3310ae8af27f16e217909721f978a0
Erwan Daubert <erwan.daubert@gmail.com>
2021-08-02 10:25:02 +0200

    Prise en compte de la review

--- backend/app/controllers/role.controller.js
+++ backend/app/controllers/role.controller.js
@@ -5,9 +5,7 @@ const ROLES = db.ROLES;
 exports.findAll = async (req, res) => {
   let roles = await req.user.getRoles();
   roles = roles.map(r => r.name)
-  console.log(roles, ROLES)
   if (roles.includes('admin')) {
-    console.log(ROLES)
     res.json(ROLES)
   } else {
     res.json([ROLES[0]])
